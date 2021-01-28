a83e2a5c599afc0806d078d91421229e4b832838
fikryans <fikriansyah.hq@gmail.com>
2021-01-28 09:48:22 +0700

    add backend authentification

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/login/Login'
+import Signup from './components/signup/Signup'
+import Dashboard from './components/dashboard/Dashboard'
+import Root from './Root'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Root>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/login' component={Login} />
+            <Route path='/signup' component={Signup} />
+            <Route path='/dashboard' component={Dashboard} />
+            <Route path="*">Ups</Route>
+          </Switch>
+        </Root>
+      </div>
+    )
+  }
 }
-
-export default App;
